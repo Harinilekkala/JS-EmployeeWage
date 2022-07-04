3b39914e474bc4ca782b3646efa6a414cc3b9288
unknown <harini.lekkala1997@gmail.com>
2022-07-04 15:00:48 +0530

    [Harini]: UC - 1 check Employee Status

---
+++ VSCode_EmpWage.js
@@ -0,0 +1,17 @@
+
+//-----UC-1___To Check Status
+{
+    const IS_ABSENT = 0
+    let empCheck = Math.floor(Math.random() * 10) % 2;
+    if  (empCheck ==  IS_ABSENT) 
+    {
+        console.log("UC-1  Employee is Absent.");
+    }
+    else
+    {
+        console.log("UC-1  Employee is Present");
+    }
+
+}
+
+
