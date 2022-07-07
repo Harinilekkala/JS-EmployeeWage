2e740c58b64f5de84749b3f645742f84d088f34d
unknown <harini.lekkala1997@gmail.com>
2022-07-07 20:46:22 +0530

    [Harini]: Creating Regex PAttern Along With Try , CAtch

--- VSCode_EmpPayroll.js
+++ VSCode_EmpPayroll.js
@@ -1,5 +1,7 @@
 // UC - 11 Creating Employee Payroll Details
 // UC - 12 Creating DAte And Gender
+// UC - 13 Implementing Concept On Regex Pattern ------------ Using Try And Catch
+
 class EmployeePayrollData
    // property
    {
@@ -26,26 +28,53 @@ class EmployeePayrollData
     {
         this._name = name;
     }
+
+    set name(name)
+    {
+        let nameCheck = RegExp('^[A-Z]{1,}[a-zA-Z]{2,}$')
+        if(nameCheck.test(name))
+        {
+            this._name = name;
+        }
+        else
+        {
+            throw "\n\n-------Raising Exception--------\n\nValiadting the name in given regular expresion to follow the rule : must have \n1.Must have capital letter \n2.Atleast 3 characters\n";
+        }
+        
+    }
     
     // method
     DisplayEmpDetails()
     {
-        const DateFormat = { year: 'numeric' , month: 'long' , day: 'numeric'};
-        const empDate = this.startDate === undefined ? "undefined" :
-                        this.startDate.toLocaleDateString("en-US" , DateFormat)
         return "id= " + this.id + "\t\tName: " + this.name + "\t\tSalary: " + this.salary + 
-                "\t\tGender: " + this.gender +"\t\tStartDate : " +empDate;
+                "\t\tGender: " + this.gender +"\t\tStartDate : " +this.startDate;
     }
 }
 {
-    let empdata = new EmployeePayrollData(1, "Harini", 20000 , 'F');
+    const date = new Date();
+    let empdata = new EmployeePayrollData(1, "Sony", 20000 , 'F');
     console.log(empdata.DisplayEmpDetails());
-    empdata.id = 2;
-    empdata.name = "Haritha";
-    empdata.salary = 30000;
-    empdata.gender = 'F'
-    console.log(empdata.DisplayEmpDetails());
-    let empdata2 = new EmployeePayrollData(3, "Harshini", 40000 , 'F' , new Date());
+    try
+    {
+        console.log("\n\n-------------------------Displaying Valid Regex-----------------------------");
+        empdata.id = 2;
+        empdata.name = "Sai"; //The Regex Is Satisfying
+        empdata.salary = 90000;
+        console.log(empdata.DisplayEmpDetails());
+
+        empdata.id = 5;
+        empdata.name = "bai"; //The Regex Is Not Satisfying
+        empdata.salary = 80000;
+        console.log(empdata.DisplayEmpDetails());
+
+
+    }
+    catch(ex)
+    {
+        console.error(ex);
+    }  
+
+    let empdata2 = new EmployeePayrollData( 3, "Harshini", 40000 , 'F' , new Date());
     let empdata3 = new EmployeePayrollData(4, "Hardin", 40000 , 'F' , new Date());
     console.log(empdata2.DisplayEmpDetails());
     console.log(empdata3.DisplayEmpDetails());   
